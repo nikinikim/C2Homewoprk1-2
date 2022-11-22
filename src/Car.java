ea7d74def999a0fe8f01601c9cf0feab5ab07af5
mosunov.nn <colt92@inbox.ru>
2022-11-22 12:16:15 +0500

    Сделано задание 1-3

--- src/Car.java
+++ src/Car.java
@@ -1,18 +1,42 @@
 public class Car {
-    private String brand;
-    private String model;
-    private double engineVolume;
+    private final String brand;
+    private final String model;
+    private final double engineVolume;
     private String color;
     private int year;
     private String country;
 
     Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if ("".equals(brand) || brand == null) {
+            this.brand = "default";
+        }else {
+            this.brand = brand;
+        }
+        if ("".equals(model) || model == null) {
+            this.model = "default";
+        }else {
+            this.model = model;
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        }else {
+            this.engineVolume = engineVolume;
+        }
+        if ("".equals(color) || color == null) {
+            this.color = "белый";
+        }else {
+            this.color = model;
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        }else {
+            this.year = year;
+        }
+        if ("".equals(country) || country == null) {
+            this.country = "default";
+        }else {
+            this.country = country;
+        }
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Car lada = new Car("Lada", "Granta", 1.7, "жёлтый", 2015, "Россия");
         Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "чёрный", 2020, "Германия");
-        Car bmw = new Car("BMW", "Z8", 3.0, "чёрный", 2021, "Германия");
+        Car bmw = new Car("BMW", "Z8", 3.0, "чёрный", 0, "Германия");
         Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
         Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
 
